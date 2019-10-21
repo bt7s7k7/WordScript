c5ebb57cad6679f7b56328f340bcb3d78bde86e0
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-10-21 18:17:02 +0200

    Type and conversion registration

---
+++ WordScript/WordScript.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace WordScript {
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public sealed class TypeNameAttribute : Attribute {
+		public Type targetType;
+
+		public TypeNameAttribute(Type targetType) {
+			this.targetType = targetType;
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public sealed class TypeConversionAttribute : Attribute { }
+
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+	public sealed class FunctionDefinitionAttribute : Attribute {
+		string name;
+
+		public FunctionDefinitionAttribute(string name) {
+			this.name = name;
+		}
+	}
+
+	public struct Function {
+		public Func<object[], object> function;
+		public Type returnType;
+		public Type[] arguments;
+		public string name;
+	}
+
+
+	[Serializable]
+	public class TypeNotRegisteredException : Exception {
+		public TypeNotRegisteredException() { }
+		public TypeNotRegisteredException(string message) : base(message) { }
+		public TypeNotRegisteredException(string message, Exception inner) : base(message, inner) { }
+		protected TypeNotRegisteredException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+
+
+	[Serializable]
+	public class FunctionNotFoundException : Exception {
+		public FunctionNotFoundException() { }
+		public FunctionNotFoundException(string message) : base(message) { }
+		public FunctionNotFoundException(string message, Exception inner) : base(message, inner) { }
+		protected FunctionNotFoundException(
+		  System.Runtime.Serialization.SerializationInfo info,
+		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+
+	public class TypeInfoProvider {
+		private static Dictionary<Type, string> names;
+		private static Dictionary<string, Function> functions;
+		private static Dictionary<string, List<string>> overloads;
+
+		protected void AddOverload(string name, string signature) {
+			if (overloads.TryGetValue(name, out List<string> signatures)) {
+				signatures.Add(signature);
+			} else {
+				overloads.Add(name, new List<string> { signature });
+			}
+		}
+
+		public void AddFunction(Function func) {
+			string signature = GetFunctionSignature(func.name, func.arguments);
+			functions.Add(signature, func);
+			AddOverload(func.name, signature);
+		}
+
+		public string GetFunctionSignature(string name, Type[] arguments) {
+			return name + " " + string.Join(" ", arguments.Select(v => names[v]));
+		}
+
+		public string GetTypeName(Type type) {
+			if (names.TryGetValue(type, out string name)) {
+				return name;
+			} else {
+				throw new TypeNotRegisteredException("Type " + name + " is not registered");
+			}
+		}
+
+		public Function GetFunction(string name) {
+			if (functions.TryGetValue(name, out Function function)) {
+				return function;
+			} else throw new FunctionNotFoundException("Function " + name + " was not found");
+		}
+
+		public List<string> GetOverloads(string name) {
+			if (overloads.TryGetValue(name, out List<string> signatures)) {
+				return signatures;
+			} else {
+				return new List<string> { };
+			}
+		}
+
+		public TypeInfoProvider() {
+			if (names == null) {
+				names = new Dictionary<Type, string>();
+				functions = new Dictionary<string, Function>();
+				overloads = new Dictionary<string, List<string>>();
+				var methods = AppDomain.CurrentDomain.GetAssemblies().AsParallel()
+					.SelectMany((v) => v.GetTypes())
+					.SelectMany(v => v.GetMethods())
+					.Where(v => v.IsStatic && !v.IsGenericMethodDefinition && !v.IsGenericMethod);
+
+				foreach (var method in methods) {
+					var nameAttr = method.GetCustomAttribute<TypeNameAttribute>(false);
+					if (nameAttr != null) {
+						var name = (string)method.Invoke(null, new object[] { });
+
+						names.Add(nameAttr.targetType, name);
+					}
+				}
+
+				foreach (var method in methods) {
+					var convAttr = method.GetCustomAttribute<TypeConversionAttribute>(false);
+					if (convAttr != null) {
+						var source = method.GetParameters()[0]?.ParameterType;
+						if (source == null) throw new Exception("Conversion method does not accept a parameter");
+						var target = method.ReturnType;
+
+						AddFunction(new Function {
+							name = GetTypeName(target) + ".from",
+							arguments = new Type[] { source },
+							returnType = target,
+							function = (args) => method.Invoke(null, args)
+						});
+					}
+				}
+
+				foreach (var method in methods) {
+					var funcDefAttrs = method.GetCustomAttributes<FunctionDefinitionAttribute>(false);
+					foreach (var funcDefAttr in funcDefAttrs) {
+						var returnType = method.ReturnType;
+						var arguments = method.GetParameters().Select(v => v.ParameterType);
+					}
+				}
+
+			}
+
+
+		}
+	}
+
+	public static class DefaultTypeInfo {
+		[TypeName(typeof(string))]
+		public static string GetStringName() => "string";
+
+		[TypeName(typeof(int))]
+		public static string GetIntName() => "int";
+	}
+
+
+}
---
+++ WordScriptTests/WorldScript.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordScript;
+
+namespace WordScriptTests {
+	/// <summary>
+	/// Summary description for WorldScript
+	/// </summary>
+	[TestClass]
+	public class WorldScript {
+		public WorldScript() {
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+		TypeInfoProvider provider = new TypeInfoProvider();
+
+		[TestMethod]
+		public void GettingDefaultTypeName() {
+			Assert.AreEqual(provider.GetTypeName(typeof(string)), "string");
+		}
+
+		public class TestClass {
+			public int testValue;
+
+			[TypeName(typeof(TestClass))]
+			public static string GetTypeName() => "TestClass";
+
+			[TypeConversion]
+			public static int ToInt(TestClass inp) => inp.testValue;
+			
+			[TypeConversion]
+			public static TestClass FromInt(int inp) => new TestClass { testValue = inp };
+		}
+
+		[TestMethod]
+		public void GettingCustomTypeName() {
+			Assert.AreEqual(provider.GetTypeName(typeof(TestClass)), TestClass.GetTypeName());
+		}
+
+		[TestMethod]
+		public void CustomConversion() {
+			var overloads = provider.GetOverloads("TestClass.from");
+			Assert.AreEqual(overloads.Count, 1, "Function signature not found, error in GetOverloads or [TypeConversion]");
+			var function = provider.GetFunction(overloads[0]);
+			Assert.AreEqual(function.returnType, typeof(TestClass), "Function return value is not correct. Either a different overload is registered or error");
+		}
+	}
+}
